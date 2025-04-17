cfd83576cebe47d3ba29be9a300455bc679b0ce0
gar3thcar3y <168173753+gar3thcar3y@users.noreply.github.com>
2025-04-17 11:44:06 +0100

    storing temps
    storing temperature in an array

--- task2/sketch_2/sketch_2.ino
+++ task2/sketch_2/sketch_2.ino
@@ -10,12 +10,20 @@ void setup() {
   Serial.begin(9600);
 }
 
+float temp_values_TD[10];
+float temp_values_FD[10];
+int current_index = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
+  
   voltage = (float) (5 / 1024.0) * (analogRead(pin)-1);
   Serial.println(voltage);
   temperature = 1.0/(log(voltage/5.0)/thermistor_val+1/298.15)-273.15;
   Serial.println(temperature);
   delay(1000);
-  
+  temp_values_TD[current_index] = temperature;
+  for(int i=0;i<10;i++){Serial.println(temp_values_TD[i]);}
+  current_index += 1;
+  if (current_index >= 10){current_index = 0;}
 }
